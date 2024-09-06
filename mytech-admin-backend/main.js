e816bdc5faa320b7c9aabbfa4e51e00096769cf0
rajni <rajniviramgama@gmail.com>
2024-09-06 19:33:36 +0530

    check commit

--- mytech-admin-backend/main.js
+++ mytech-admin-backend/main.js
@@ -26,5 +26,5 @@ app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
   
